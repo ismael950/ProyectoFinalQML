99886d827b3fe613a9810e2cfa38dbc573087211
ismael950 <govi041009@gmail.com>
2025-05-20 08:52:54 -0700

    almost finished DS generator

--- DS_generatior.py
+++ DS_generatior.py
@@ -1,11 +1,12 @@
 #Creation of electronic Hamiltonians of LiH, H4 (rectangle) and H4(line)
 
 #Atomic distances:
-    #LiH and H4(line) [0.5A, 3.3A]
-    #H4(rectangle) [0.5A, 2.0A]x[0.5A, 2.0A]
+    #LiH [0.5A, 3.3A]
 
 #Use PySCF and OpenFermion
 import numpy as np
+import pyscf
+from pyscf.scf.dhf import dip_moment
 from openfermion.chem import MolecularData as md
 from openfermionpyscf import run_pyscf
 from openfermion.hamiltonians import generate_hamiltonian
@@ -21,6 +22,8 @@ bas = 'sto-3g'
 #LiH 
 LiH_ham = []
 LiH_dis = np.linspace(0.5, 3.3, 100)
+LiH_molecules = []
+
 for d in LiH_dis:
     molecule = md(
         geometry = [['Li', (0, 0, 0)], ['H', (0, 0, d)]], 
@@ -29,6 +32,7 @@ for d in LiH_dis:
     )
 
     mol = run_pyscf(molecule, run_scf=1, run_mp2=0, run_cisd=0, run_ccsd=0, run_fci=0)
+    LiH_molecules.append(mol)
 
     reduced_one_body = mol.one_body_integrals[:4, :4]
     reduced_two_body = mol.two_body_integrals[:4, :4, :4, :4]
@@ -41,73 +45,46 @@ for d in LiH_dis:
 
     LiH_ham.append(hamiltonian)
 
-#H4 lin
-H4lin_ham = []
-H4lin_dis = np.linspace(0.5, 3.3, 100)
-for d in H4lin_dis:
-    molecule = md(
-        geometry = [['H', (0, 0, 0)], ['H', (0, 0, d)], ['H', (0, 0, 2*d)], ['H', (0, 0, 3*d)]], 
-        basis = bas,
-        multiplicity = 1
-    )
-
-    mol = run_pyscf(molecule, run_scf=1, run_mp2=0, run_cisd=0, run_ccsd=0, run_fci=0)
-    
-    hamiltonian = generate_hamiltonian(
-        one_body_integrals=mol.one_body_integrals,
-        two_body_integrals=mol.two_body_integrals,
-        constant=mol.nuclear_repulsion
-    )
-
-    H4lin_ham.append(hamiltonian)
-
-#H4 rec
-H4rec_ham = []
-H4rec_dis = np.linspace(0.5, 2.0, 10)
-for d1 in H4rec_dis:
-    for d2 in H4rec_dis:
-        molecule = md(
-            geometry = [['H', (0, 0, 0)], ['H', (d1, 0, 0)], ['H', (0, d2, 0)], ['H', (d1, d2, 0)]], 
-            basis = bas,
-            multiplicity = 1
-        )
-
-        mol = run_pyscf(molecule, run_scf=1, run_mp2=0, run_cisd=0, run_ccsd=0, run_fci=0)
-        
-        hamiltonian = generate_hamiltonian(
-            one_body_integrals=mol.one_body_integrals,
-            two_body_integrals=mol.two_body_integrals,
-            constant=mol.nuclear_repulsion
-        )
-
-        H4rec_ham.append(hamiltonian)
-
-#Jordan-Wigner transformation
 for i in range(len(LiH_ham)):
     LiH_ham[i] = jordan_wigner(LiH_ham[i])
 
-for i in range(len(H4lin_ham)):
-    H4lin_ham[i] = jordan_wigner(H4lin_ham[i])
-    
-for i in range(len(H4rec_ham)):
-    H4rec_ham[i] = jordan_wigner(H4rec_ham[i])    
-
-#For noiseless simulation, GS of H(r) is prepared by exact diagonalization
 from openfermion.linalg import get_sparse_operator
 from scipy.sparse.linalg import eigsh
 
+#LiH
 LiH_eigenvalues = []
 LiH_eigenvectors = []
 
-#LiH
+for H in LiH_ham:
+    SH = get_sparse_operator(H)
+    evals, evcts = eigsh(SH, k=3, which = 'SA')
+    LiH_eigenvalues.append(evals)
+    GS=[]
+    for i in evcts:
+        GS.append(i[0])
+    LiH_eigenvectors.append(GS)
 
 
+#Compute quantities of excited state properties to be predicted (formula 2) deltaE1 and deltaE2
+deltas = []
+d1min = 1000000000
+d1max = -1000000000
+d2min = 1000000000
+d2max = -1000000000
 
-#Compute quantities of excited state properties to be predicted (formula 2)
-
-#Diagonalization of H(r) 
+for eivls in LiH_eigenvalues:
+    d1 = eivls[1]-eivls[0]
+    d2 = eivls[2]-eivls[0]
+    d1min = min(d1min, d1)
+    d1max = max(d1max, d1)
+    d2min = min(d2min, d2)
+    d2max = max(d2max, d2)
+    deltas.append([d1, d2])
 
 #Fit values into [-1, 1] range
+for i in range(len(deltas)):
+    deltas[i][0] = 2*(deltas[i][0]-d1min)/(d1max-d1min)-1
+    deltas[i][1] = 2*(deltas[i][1]-d2min)/(d2max-d2min)-1
 
 #30 training data points and 50 test points for LiH
 
