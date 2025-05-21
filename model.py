d4dc9c0918a48e1bafcb857668c1cc90aa8826e0
ismael950 <govi041009@gmail.com>
2025-05-21 08:42:25 -0700

    changes

--- model.py
+++
@@ -1,7 +0,0 @@
-# psi_0 est, y(r) are training set and test set for evaluation
-
-#Entangler is the time-evolution operator e^(iH_TFIM*T), T = 10, can be in NISQ devices
-
-#Classical machine learning unit - linear regression 
-
-#
